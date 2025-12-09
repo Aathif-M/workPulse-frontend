3a283e0c1bae181a9486ebcfdb524cce950acac3
Mohamed Aathif <aathifm99@gmail.com>
2025-12-09 10:15:49 +0530

    Corrections to axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://api.workpulse.us';
+
 const api = axios.create({
-    baseURL: `https://api.workpulse.us`,
+    baseURL: BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
